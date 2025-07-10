46cde383cd36cf2c1b7beaf4f0ae4302216701c6
Preston Osoro <prestonosoro56@gmail.com>
2025-07-10 03:38:49 +0300

    chore(types): add TypeScript declarations for <rapi-doc> web component

---
+++ types/rapidoc.d.ts
@@ -0,0 +1,7 @@
+declare module 'rapidoc';
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    'rapi-doc': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+  }
+}
